396c5e338546b7c0f913cefbd7a7692b5dd0d42b
Bossolin <vladyslav.okipnyak@icloud.com>
2021-11-01 11:07:17 +0100

    added responsive box size

--- script.js
+++ script.js
@@ -13,10 +13,14 @@ function newGrid() {
 }
 
 const createGrid = () => {
-  let input = prompt("Choose the size of the grid (up to 100) ", "");
+  let input = prompt("Choose the size of the grid (up to 100 per side) ", "");
+  input > 100 ? alert("The amount of squares is too damn high!") : "";
+
   for (let i = 0; i < input * input; i++) {
     const div = document.createElement("div");
     div.classList.add("box");
+    div.style.width = `${100 / input}%`;
+    div.style.height = `${100 / input}%`;
     container.appendChild(div);
     div.addEventListener("mouseover", () => div.classList.add("etched"));
     resetButton.addEventListener("click", () => div.classList.remove("etched"));
